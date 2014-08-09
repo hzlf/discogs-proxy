3768f3708631fb6bd7e6cfbdfb16c49af49226c5
ohrstrom <jonas@anorg.net>
2014-08-09 14:13:16 +0200

    template cleanup

--- website/base/static/js/stats.js
+++ website/base/static/js/stats.js
@@ -1,5 +1,6 @@
 /*
  * Proxy statistics
+ * This is just a really quick and dirty implementation - _for demonstration purpose only_.
  */
 
 ProxyStatsApp = function () {
@@ -7,35 +8,27 @@ ProxyStatsApp = function () {
     var self = this;
     this.apiUrl;
     this.container;
-    this.interval = 5000;
+    this.interval = 30000;
 
     this.init = function () {
-
         self.load()
         if(self.interval){
             setInterval(function () {
                 self.load();
             }, self.interval);
         }
-
-    }
+    };
 
 
     this.load = function() {
         $.get(self.apiUrl, function(data) {
              self.display(data);
         });
-    }
+    };
 
     this.display = function(data) {
-
         $.each(data, function(k, v) {
-            console.log('k', k, 'v', v);
-
             $('#stats_' + k, self.container).html(v)
-
         })
-
-    }
-
-}
+    };
+};
